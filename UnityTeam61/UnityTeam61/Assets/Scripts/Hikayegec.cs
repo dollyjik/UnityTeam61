fc4845a6626f00b96418a8060531d48f8b246d83
ahmet19830 <Adeveci15@ku.edu.tr>
2024-07-30 10:50:37 +0300

    Her bir seviyeye diyalog eklendi
    Devam butonu hata verdiği için boşluk tuşu yani space tuşu ile hikayelerin geçilmesi sağlandı. Ana hikaye ingilizceye çevirildi fakat seviye 3 kaldırılacaksa diyalog ve hikaye baştan gözden geçirilmeli.

--- UnityTeam61/UnityTeam61/Assets/Scripts/Hikayegec.cs
+++ UnityTeam61/UnityTeam61/Assets/Scripts/Hikayegec.cs
@@ -3,24 +3,32 @@ using TMPro;
 
 public class PageTurner : MonoBehaviour
 {
-    public TextMeshProUGUI[] pages; // Tüm sayfalarý buraya atayýn
-    public GameObject panel; // Panelin GameObject'i
+    public TextMeshProUGUI[] pages; 
+    public GameObject panel; 
     private int currentPageIndex = 0;
 
     public void NextPage()
     {
-        // Mevcut sayfayý kapat
+        
         pages[currentPageIndex].gameObject.SetActive(false);
 
-        // Son sayfaya gelindiyse paneli kapat
+        
         if (currentPageIndex == pages.Length - 1)
         {
             panel.SetActive(false);
             return;
         }
+        
 
-        // Bir sonraki sayfaya geç
+        
         currentPageIndex++;
         pages[currentPageIndex].gameObject.SetActive(true);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextPage();
+        }
     }
 }
